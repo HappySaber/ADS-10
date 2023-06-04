bbc21b4601313f1c7370328880cdd88d90742291
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-06-04 23:18:16 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,51 @@
+#pragma once
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <algorithm>
+#include <iostream>
+#include <vector>
+struct Node {
+  char value;
+  bool isRoot = false;
 
+  std::vector<Node*> ptrs;
+};
+class Tree {
+ private:
+  Node* root;
+  std::vector<std::vector<char>> permutations;
+  void findPermutations(Node* root, std::vector<char> vect) {
+    if (!root->isRoot) vect.push_back(root->value);
+    if (!root->ptrs.empty()) {
+      for (Node* child : root->ptrs) {
+        findPermutations(child, vect);
+      }
+    } else {
+      permutations.push_back(vect);
+    }
+  }
+  void insert(Node* root, const std::vector<char>& vect) {
+    for (char c : vect) {
+      Node* temp = new Node;
+      temp->value = c;
+      root->ptrs.push_back(temp);
+      std::vector<char> otherChars(vect);
+      otherChars.erase(std::find(otherChars.begin(), otherChars.end(), c));
+      insert(temp, otherChars);
+    }
+  }
+
+ public:
+  explicit Tree(const std::vector<char>& vect) {
+    root = new Node;
+    root->isRoot = true;
+    insert(root, vect);
+    std::vector<char> actual;
+    findPermutations(root, actual);
+  }
+  std::vector<std::vector<char>> getPermutations() const {
+    return permutations;
+  }
+};
 #endif  // INCLUDE_TREE_H_
